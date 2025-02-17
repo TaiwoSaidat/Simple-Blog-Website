78275b5fc40b36d3f53ae85c09edea6a665312a0
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2025-02-17 16:24:17 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-
 const Footer = () => {
   return (
     <>
       <div className="  w-full text-center " id="footer">
-        <hr className="bg-gray-300" />
-        <div className=" flex justify-center my-4 ">
-          <p>All rights reserved &copy;2024.  </p>
-          <a href="https://github.com/TaiwoSaidat/">TaiwoSaidat</a>
+        <hr className="bg-gray-300 my-4" />
+        <div className="flex flex-col  ">
+          <div className=" flex justify-center  ">
+            <p>All rights reserved &copy;2024. </p>
+            <a href="https://github.com/TaiwoSaidat/Simple-Blog-Website">
+              TaiwoSaidat.
+            </a>
+          </div>
+          <p >Built using React Vite and Firebase</p>
         </div>
       </div>
     </>
